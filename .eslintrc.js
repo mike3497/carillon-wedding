564a17a00ff48649bc6d68ec4f9ce3a9e47f36ce
Michael Carillon <mcarillon97@gmail.com>
2023-06-19 19:43:16 -0400

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+};
